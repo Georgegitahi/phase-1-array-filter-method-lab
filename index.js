9cede1796b378c9e30295eccacd23afdccc88505
Georgegitahi <petergeorgembugua724@gmail.com>
2024-04-04 16:08:47 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers,Bobby){
+    return drivers.filter(
+        (possibleMatch)=>possibleMatch.toLowerCase() === Bobby.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers,driversLength){
+     return drivers.filter(
+        (possibleMatch)=>
+        possibleMatch.toLowerCase().indexOf(driversLength.toLowerCase()) === 0
+     );
+}
+
+
+function matchName(drivers,hometown){
+    return drivers.filter((record)=>record.name=== hometown)
+}
